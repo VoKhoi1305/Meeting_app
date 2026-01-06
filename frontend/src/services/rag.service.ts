672b53a310a312ee8d9b0303d01b6e0fa6dd0431
Vo Anh Khoi <voanhkhoi91b@gmail.com>
2026-01-07 00:01:40 +0700

    cho toi di ngu

--- frontend/src/services/rag.service.ts
+++ frontend/src/services/rag.service.ts
@@ -1,20 +1,4 @@
-// import api from './api';
 
-// export const ragService = {
-//   uploadFile: async (file: File) => {
-//     const formData = new FormData();
-//     formData.append('file', file);
-//     const response = await api.post('/rag/upload', formData, {
-//       headers: { 'Content-Type': 'multipart/form-data' },
-//     });
-//     return response.data;
-//   },
-
-//   chat: async (question: string) => {
-//     const response = await api.post('/rag/chat', { question });
-//     return response.data;
-//   },
-// };
 
 import api from './api';
 
